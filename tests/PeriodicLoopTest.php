51d49229badc53c9861173337dbc8b56a5f054d8
Wizard Loop <67387949+WizardLoop@users.noreply.github.com>
2025-07-01 16:46:06 +0300

    Update PeriodicLoopTest.php

--- tests/PeriodicLoopTest.php
+++ tests/PeriodicLoopTest.php
@@ -3,27 +3,61 @@
 use PHPUnit\Framework\TestCase;
 use WizardLoop\Loop\PeriodicLoop;
 use function Amp\async;
+use function Amp\delay;
 
 class PeriodicLoopTest extends TestCase
 {
     public function testPeriodicExecution()
     {
         $calls = 0;
-        $loop = new PeriodicLoop(0.01, function () use (&$calls) { $calls++; }, null, null, 3);
+        $loop = new PeriodicLoop(0.001, function () use (&$calls) { $calls++; }, null, null, 3);
 
-        async(function () use ($loop) {
-            $loop->start();
-            \Amp\delay(0.05);
-            $loop->stop();
-        });
+        $loop->start();
+        delay(0.01)->await(); 
+        $loop->stop();
 
-        $this->assertLessThanOrEqual(3, $calls);
+        $this->assertGreaterThan(0, $calls, "PeriodicLoop should execute callback at least once");
+        $this->assertLessThanOrEqual(3, $calls, "PeriodicLoop should not execute more than maxTicks");
+    }
+
+    public function testOnTickCallback()
+    {
+        $ticks = 0;
+        $loop = new PeriodicLoop(0.001, function () {}, function () use (&$ticks) { $ticks++; }, null, 2);
+
+        $loop->start();
+        delay(0.005)->await();
+        $loop->stop();
+
+        $this->assertGreaterThan(0, $ticks, "onTick callback should be called at least once");
+    }
+
+    public function testErrorHandling()
+    {
+        $errorCalled = false;
+        $loop = new PeriodicLoop(0.001, function () { throw new \Exception("fail"); }, null, function () use (&$errorCalled) { $errorCalled = true; }, 1);
+
+        $loop->start();
+        delay(0.002)->await();
+        $loop->stop();
+
+        $this->assertTrue($errorCalled, "onError callback should be called when an exception occurs.");
+    }
+
+    public function testStartStopIdempotence()
+    {
+        $loop = new PeriodicLoop(0.001, function () {});
+        $loop->start();
+        $loop->start(); 
+        $loop->stop();
+        $loop->stop(); 
+        $this->assertFalse($loop->isRunning());
     }
 
     public function testCronSyntax()
     {
         $this->expectException(\InvalidArgumentException::class);
-
-        $loop = new PeriodicLoop('*', function () { });
+        
+        new PeriodicLoop('*', function () {});
     }
 }
