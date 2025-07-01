22a7eacc4d3432b3712c039ab2a6173bb566ceba
Wizard Loop <67387949+WizardLoop@users.noreply.github.com>
2025-07-01 18:43:27 +0300

    Update GenericLoopTest.php

--- tests/GenericLoopTest.php
+++ tests/GenericLoopTest.php
@@ -4,12 +4,50 @@ use PHPUnit\Framework\TestCase;
 use WizardLoop\Loop\GenericLoop;
 use Amp\Future;
 use function Amp\async;
+use function Amp\delay;
 
 class DummyLoop extends GenericLoop
 {
+    private $shouldThrow = false;
+    private $ticks = 0;
+    private $maxTicks = 2;
+
+    public function __construct($shouldThrow = false, $maxTicks = 2)
+    {
+        $this->shouldThrow = $shouldThrow;
+        $this->maxTicks = $maxTicks;
+    }
+
     protected function runLoop(): Future
     {
-        return async(fn() => null);
+        $this->deferred = new \Amp\DeferredFuture();
+        async(function () {
+            $this->ticks = 0;
+            while ($this->running && $this->ticks < $this->maxTicks) {
+                if ($this->paused) {
+                    yield delay(0.001);
+                    continue;
+                }
+                try {
+                    if ($this->shouldThrow) {
+                        throw new \Exception("fail!");
+                    }
+                    if ($this->onTick) {
+                        ($this->onTick)();
+                    }
+                } catch (\Throwable $e) {
+                    if ($this->onError) {
+                        ($this->onError)($e);
+                    }
+                }
+                $this->ticks++;
+                yield delay(0.001);
+            }
+            if ($this->deferred && !$this->deferred->isComplete()) {
+                $this->deferred->complete(null);
+            }
+        });
+        return $this->deferred->getFuture();
     }
 }
 
@@ -17,53 +55,73 @@ class GenericLoopTest extends TestCase
 {
     public function testStartStop()
     {
-        $loop = new DummyLoop();
-        $this->assertFalse($loop->isRunning());
-        $loop->start();
-        $this->assertTrue($loop->isRunning());
-        $loop->stop();
-        $this->assertFalse($loop->isRunning());
+        $loop = new DummyLoop(false, 1);
+
+        async(function () use ($loop) {
+            $loop->start();
+            yield delay(0.005);
+            $loop->stop();
+        })->await();
+
+        $this->assertFalse($loop->isRunning(), "Loop should not be running after stop()");
     }
 
     public function testEventHooks()
     {
-        $loop = new DummyLoop();
-
         $started = false;
+        $ticked = false;
         $stopped = false;
-        $loop->onStart(function () use (&$started) {
-            $started = true;
-        });
-        $loop->onStop(function () use (&$stopped) {
-            $stopped = true;
-        });
 
-        $loop->start();
-        $this->assertTrue($started);
+        $loop = new DummyLoop(false, 1);
+        $loop->onStart(function () use (&$started) { $started = true; });
+        $loop->onTick(function () use (&$ticked) { $ticked = true; });
+        $loop->onStop(function () use (&$stopped) { $stopped = true; });
 
-        $loop->stop();
-        $this->assertTrue($stopped);
+        async(function () use ($loop) {
+            $loop->start();
+            yield delay(0.005);
+            $loop->stop();
+        })->await();
+
+        $this->assertTrue($started, "onStart should be called");
+        $this->assertTrue($ticked, "onTick should be called");
+        $this->assertTrue($stopped, "onStop should be called");
     }
 
     public function testErrorHandling()
     {
-        $loop = new DummyLoop();
-        $errorCalled = false;
-
-        $loop->onError(function () use (&$errorCalled) {
-            $errorCalled = true;
+        $errorCaught = false;
+        $loop = new DummyLoop(true, 1);
+        $loop->onError(function ($e) use (&$errorCaught) {
+            $errorCaught = true;
+            $this->assertInstanceOf(\Exception::class, $e);
         });
 
-        $reflection = new \ReflectionClass($loop);
-        if ($reflection->hasProperty('onError')) {
-            $prop = $reflection->getProperty('onError');
-            $prop->setAccessible(true);
-            $cb = $prop->getValue($loop);
-            if (is_callable($cb)) {
-                $cb(new \Exception('test'));
-            }
-        }
+        async(function () use ($loop) {
+            $loop->start();
+            yield delay(0.005);
+            $loop->stop();
+        })->await();
 
-        $this->assertTrue($errorCalled, "onError callback should be called.");
+        $this->assertTrue($errorCaught, "onError callback should be called when exception is thrown");
+    }
+
+    public function testPauseResume()
+    {
+        $ticks = 0;
+        $loop = new DummyLoop(false, 3);
+        $loop->onTick(function () use (&$ticks) { $ticks++; });
+
+        async(function () use ($loop) {
+            $loop->start();
+            yield delay(0.002);
+            $loop->pause();
+            $countAtPause = $ticks;
+            yield delay(0.005);
+            $loop->resume();
+            yield delay(0.005);
+            $loop->stop();
+            $this->assertEquals($countAtPause, $ticks, "No new ticks should happen during pause");
+        })->await();
     }
 }
