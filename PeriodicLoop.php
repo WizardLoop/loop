c4dcec448b0b54181ec2af16efd33a3ef89e0261
Wizard Loop <67387949+WizardLoop@users.noreply.github.com>
2025-07-01 18:38:56 +0300

    Update PeriodicLoop.php

--- PeriodicLoop.php
+++ PeriodicLoop.php
@@ -10,28 +10,11 @@ use Cron\CronExpression;
 
 class PeriodicLoop extends GenericLoop
 {
-    /**
-     * @var float|callable|string
-     */
     private $interval;
     private $callback;
     private $maxTicks = null;
     private $tickCount = 0;
     private $cron = null;
-
-    public function __construct($interval, callable $callback, callable $onTick = null, callable $onError = null, ?int $maxTicks = null)
-    {
-        if (is_string($interval)) {
-            $this->cron = CronExpression::factory($interval);
-            $this->interval = $interval;
-        } else {
-            $this->interval = $interval;
-        }
-        $this->callback = $callback;
-        $this->onTick = $onTick;
-        $this->onError = $onError;
-        $this->maxTicks = $maxTicks;
-    }
 
     protected function runLoop(): Future
     {
@@ -55,8 +38,6 @@ class PeriodicLoop extends GenericLoop
                 } catch (\Throwable $e) {
                     if ($this->onError) {
                         ($this->onError)($e);
-                    } else {
-                        throw $e;
                     }
                 }
                 $this->tickCount++;
@@ -64,19 +45,27 @@ class PeriodicLoop extends GenericLoop
                     $this->stop();
                     break;
                 }
-                if ($this->cron) {
-                    $now = new \DateTimeImmutable();
-                    $next = $this->cron->getNextRunDate($now);
-                    $interval = $next->getTimestamp() - $now->getTimestamp() + ($next->format('u') - $now->format('u')) / 1e6;
-                } else {
-                    $interval = is_callable($this->interval) ? ($this->interval)() : $this->interval;
-                }
+                $interval = is_callable($this->interval) ? ($this->interval)() : $this->interval;
                 yield delay(max($interval, 0.001));
             }
-            if ($this->deferred) {
+            if ($this->deferred && !$this->deferred->isComplete()) {
                 $this->deferred->complete(null);
             }
         });
         return $this->deferred->getFuture();
+    }
+
+    public function __construct($interval, callable $callback, callable $onTick = null, callable $onError = null, ?int $maxTicks = null)
+    {
+        if (is_string($interval)) {
+            $this->cron = CronExpression::factory($interval);
+            $this->interval = 1;
+        } else {
+            $this->interval = $interval;
+        }
+        $this->callback = $callback;
+        $this->onTick = $onTick;
+        $this->onError = $onError;
+        $this->maxTicks = $maxTicks;
     }
 }
